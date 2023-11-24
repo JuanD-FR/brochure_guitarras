7952100f3b7ba6c9da16e493d73d9fd529133f0d
Juan David <juanda.fero89@gmail.com>
2023-11-23 14:58:29 -1200

    base url de vite config, eliminado

--- src/GuitarApp.jsx
+++ src/GuitarApp.jsx
@@ -1,9 +1,10 @@
+import { AuthProvider } from "./auth/context/AuthProvider"
 import { AppRouter } from "./router/AppRouter"
 
 export const GuitarApp = () => {
   return (
-    <>
+    <AuthProvider>
         <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
